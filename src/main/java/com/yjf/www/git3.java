a6e6db1d560b6303e5aa12972fc2c1a4facb8c15
yao <939861608@qq.com>
2023-01-01 18:36:01 +0800

    hot-lix git3第一次提交

--- src/main/java/com/yjf/www/git3.java
+++ src/main/java/com/yjf/www/git3.java
@@ -4,5 +4,6 @@ public class git3 {
     public static void main(String[] args) {
         System.out.println("你好1");
         System.out.println("你好2");
+        System.out.println("你好3");
     }
 }
